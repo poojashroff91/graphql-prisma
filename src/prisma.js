fc47ebbc738541ecc6095f4cddd41e8733ff1c15
poshroff <poshroff@paypal.com>
2019-02-21 23:57:20 -0800

    Update mutations with prisma

--- src/prisma.js
+++ src/prisma.js
@@ -5,10 +5,44 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 });
 
-prisma.query.users(null, `{ id name email posts { id title } }` ).then((data) => {
-    console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.users(null, `{ id name email posts { id title } }` ).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
 
-prisma.query.comments(null, `{ id text author { id name } }`).then((data)=> {
+// prisma.query.comments(null, `{ id text author { id name } }`).then((data)=> {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
+
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: "GraphQL 101!",
+//         body: "",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "cjs9ye0e101w90884ynrs27xn"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then ((data) => {
+//     console.log(data);
+//     return prisma.query.users(null, '{ id name email posts { id title } }');
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// })
+
+prisma.mutation.updatePost({
+    where: {
+        id: "cjsfqj3xg002m0884d1zcyirl"
+    },
+    data: {
+        body: "This is the body of the previous post",
+        published: true
+    }
+}, '{ id title body published }').then((data) => {
+    console.log(data);
+    return prisma.query.posts(null, '{ id title body published }');
+}). then ((data)=> {
     console.log(JSON.stringify(data, undefined, 2));
-});
+})
