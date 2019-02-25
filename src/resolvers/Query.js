6a2799dd57a5606e4a5f1063c68c728c492af08b
poshroff <poshroff@paypal.com>
2019-02-24 21:42:08 -0800

    Integrating operation arguments

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,28 +1,35 @@
 const Query = {
-    users (parent, args, { db, prisma }, info) {
+    users (parent, args, { prisma }, info) {
 
-        return prisma.query.users(null, info)
+        const opArgs = {}
 
-        // if(args.query){
-        //     return db.users.filter((user) => {
-        //         return user.name.toLowerCase().includes(args.query.toLowerCase());
-        //     });
-        // } else {
-        //     return db.users;
-        // }
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                }, {
+                    email_contains: args.query
+                }]
+            }
+        }
+
+        return prisma.query.users(opArgs, info)
     },
-    posts (parent, args, { db, prisma }, info) {
+    posts (parent, args, { prisma }, info) {
 
-        return prisma.query.posts(null, info) 
-        // if(args.query){
-        //     return db.posts.filter((post) => {
-        //         const titleMatch =  post.title.toLowerCase().includes(args.query.toLowerCase());
-        //         const bodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
-        //         return titleMatch || bodyMatch;
-        //     });
-        // } else {
-        //     return db.posts;
-        // }
+        const opArgs = {}
+
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    title_contains: args.query
+                }, {
+                    body_contains: args.query
+                }]
+            }
+        }
+
+        return prisma.query.posts(opArgs, info) 
     },
     comments (parent, args, { db }, info) {
         return db.comments;
