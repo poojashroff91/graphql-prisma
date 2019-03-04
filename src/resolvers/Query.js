7933d5199a09a053a7638ea2f67114cfcee20369
poshroff <poshroff@paypal.com>
2019-03-03 22:13:54 -0800

    Locking down queries - I

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,17 @@
+import getUserId from '../utils/getUserId'
+
 const Query = {
+
+    me (parent, args, { prisma, request}, info) {
+        const userId = getUserId(request);
+
+        return prisma.query.user({
+            where: {
+                id: userId
+            }
+        })
+    },
+
     users (parent, args, { prisma }, info) {
 
         const opArgs = {}
@@ -33,6 +46,31 @@ const Query = {
     },
     comments (parent, args, { prisma }, info) {
         return prisma.query.comments(null, info)
+    },
+    async post(parent, args, {prisma, request}, info) {
+
+        const userId = getUserId(request, false);
+
+        const posts = await prisma.query.posts({
+            where: {
+                id: args.id,
+                OR: [{
+                    published: true
+                }, {
+                    author: {
+                        id: userId
+                    }
+                }]
+            }
+        }, info)
+
+        if(posts.length === 0) {
+            throw new Error('Post not found')
+        }
+
+        return posts[0]
+
+
     }
 };
 
